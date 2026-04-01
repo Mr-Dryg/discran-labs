8f107e00f980991dcbf64215db9777aa4c2a7526
Mr-Dryg <shirobockow.dim@yandex.ru>
2026-04-01 21:41:41 +0300

    insertBalance finished

--- laba-2/rbtree.cpp
+++ laba-2/rbtree.cpp
@@ -21,33 +21,32 @@ class RBTree {
         std::string key;
         unsigned long long value;
         Color color;
-        Node* left;
-        Node* right;
-        Node* parent;
+        Node* left = nullptr;
+        Node* right = nullptr;
+        Node* parent = nullptr;
 
         Node(const std::string& key, unsigned long long value, Node* parent, Color color)
-            : key(key), value(value), color(color), left(nullptr), right(nullptr), parent(parent) {}
+            : key(key), value(value), color(color), parent(parent) {}
 
-        Node* _grandpa() {
-            if (parent == nullptr) {
-                return nullptr;
-            }
+        bool isLeftSon() {
+            return parent && this == parent->left;
+        }
+
+        Node* grandpa() {
+            if (!parent) return nullptr;
             return parent->parent;
         }
 
-        bool _isLeftSon() {
-            return this == parent->left;
+        Node* bro() {
+            if (!parent) return  nullptr;
+            if (this->isLeftSon()) {
+                return parent->right;
+            }
+            return parent->left;
         }
 
-        Node* _uncle() {
-            Node* grandpa;
-            if ((grandpa = this->_grandpa()) == nullptr) {
-                return nullptr;
-            }
-            if (this->_isLeftSon()) {
-                return grandpa->right;
-            }
-            return grandpa->left;
+        Node* uncle() {
+            return parent->bro();
         }
     };
 
@@ -60,37 +59,107 @@ class RBTree {
         delete node;
     }
 
-    Node* balance(Node*& node) {
-        return nullptr;
+    Node* leftRotation(Node* node) {
+        if (!node->right) return node;
+
+        Node* subroot = node->right;
+        Node* mv_node = subroot->left;
+
+        if (node->parent) {
+            (node->isLeftSon() ? node->parent->left : node->parent->right) = subroot;
+        }
+        else {
+            root = subroot;
+        }
+        subroot->parent = node->parent;
+        subroot->left = node;
+        node->parent = subroot;
+        node->right = mv_node;
+        if (mv_node) mv_node->parent = node;
+
+        return subroot;
     }
 
-    Data::Status _add(Node*& node, const std::string& key, unsigned long long value) {
-        if (key < node->key) {
-            if (node->left == nullptr) {
-                node->left = new Node(key, value, node, Node::red);
-                balance(node->left);
-                return Data::ok;
-            }
-            return _add(node->left, key, value);
-        }
-        else if (node->key < key) {
-            if (node->right == nullptr) {
-                node->right = new Node(key, value, node, Node::red);
-                balance(node->right);
-                return Data::ok;
-            }
-            return _add(node->right, key, value);
-        }
+    Node* rightRotation(Node* node) {
+        if (!node->left) return node;
 
+        Node* subroot = node->left;
+        Node* mv_node = subroot->right;
+
+        if (node->parent) {
+            (node->isLeftSon() ? node->parent->left : node->parent->right) = subroot;
+        }
+        else {
+            root = subroot;
+        }
+        subroot->parent = node->parent;
+        subroot->right = node;
+        node->parent = subroot;
+        node->left = mv_node;
+        if (mv_node) mv_node->parent = node;
+
+        return subroot;
+    }
+
+    void insertBalance(Node* node) {
+        while (node->parent && node->parent->color == Node::red) {
+            Node* uncle = node->uncle();
+            Node* grandpa = node->grandpa();
+
+            if (uncle && uncle->color == Node::red) {
+                node->parent->color = uncle->color = Node::black;
+                grandpa->color = Node::red;
+            }
+            else if (!uncle || (uncle && uncle->color == Node::black)) {
+                if (node->parent->isLeftSon() && !node->isLeftSon()) {
+                    node = leftRotation(node->parent);
+                }
+                else if (!node->parent->isLeftSon() && node->isLeftSon()) {
+                    node = rightRotation(node->parent);
+                }
+                else {
+                    node = node->parent;
+                }
+
+                node->color = Node::black;
+                grandpa->color = Node::red;
+
+                if (node->isLeftSon()) {
+                    grandpa = rightRotation(grandpa);
+                }
+                else {
+                    grandpa = leftRotation(grandpa);
+                }
+            }
+            node = grandpa;
+        }
+        root->color = Node::black;
+    }
+
+Data::Status _insert(Node* node, const std::string& key, unsigned long long value) {
+    if (key == node->key) {
         return Data::exist;
     }
+    
+    Node*& next_node = (key < node->key) ? node->left : node->right;
+    
+    if (next_node) {
+        return _insert(next_node, key, value);
+    }
+    
+    next_node = new Node(key, value, node, Node::red);
+    if (node->color == Node::red) {
+        insertBalance(next_node);
+    }
+    return Data::ok;
+}
 
-    void _del(Node* node) {
-        // delete node and balance()
+    void _remove(Node* node) {
+        // remove node and removeBalance()
     }
 
     Node* _find(Node* node, const std::string& key) {
-        if (node == nullptr || key == node->key) {
+        if (!node || key == node->key) {
             return node;
         }
         if (key < node->key) {
@@ -108,26 +177,26 @@ public:
         destroy(root);
     }
 
-    Data::Status add(const std::string& key, unsigned long long value) {
-        if (root == nullptr) {
+    Data::Status insert(const std::string& key, unsigned long long value) {
+        if (!root) {
             root = new Node(key, value, nullptr, Node::black);
             return Data::ok;
         }
-        return _add(root, key, value);
+        return _insert(root, key, value);
     }
 
-    Data::Status del(const std::string& key) {
+    Data::Status remove(const std::string& key) {
         Node* node = _find(root, key);
-        if (node == nullptr) {
+        if (!node) {
             return Data::noSuchWord;
         }
-        _del(node);
+        _remove(node);
         return Data::ok;
     }
 
     Data find(const std::string& key) {
         Node* node = _find(root, key);
-        if (node == nullptr) {
+        if (!node) {
             return Data{.status = Data::noSuchWord};
         }
         return Data{.status = Data::ok, .value = node->value};
